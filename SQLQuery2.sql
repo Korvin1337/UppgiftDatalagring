49ad72da51a0876b37dd4343ad7c6ad5db141483
Korvin1337 <korvin1337@gmail.com>
2025-01-20 22:49:12 +0100

    query ?

---
+++ SQLQuery2.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Customers(Name, Email)
+VALUES('First2 Last2', 'First2.Last2@domain.com')
+
+INSERT INTO Projects(ProjectName, StartDate, EndDate, ProjectManager, CustomerId, TotalCost, Status)
+VALUES('Project Name', '2025-01-20', '2025-02-26', 'Project Manager', '2', '100000.00', 'Ongoing')
